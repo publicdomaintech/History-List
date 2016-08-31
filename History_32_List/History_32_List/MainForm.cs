7efa3fb4c9714cbc930346b8527922dbb914bcdd
Victor L. Senior (VLS) <support@publicdomain.tech>
2016-08-30 23:02:42 -0400

    Add initial solution files

---
+++ History_32_List/History_32_List/MainForm.cs
@@ -0,0 +1,90 @@
+﻿// <copyright file="MainForm.cs" company="www.PublicDomain.tech">All rights waived.</copyright>
+
+// Programmed by Victor L. Senior (VLS) <support@publicdomain.tech>, 2016
+//
+// Web: http://publicdomain.tech
+//
+// Sources: http://github.com/publicdomaintech/
+//
+// This software and associated documentation files (the "Software") is
+// released under the CC0 Public Domain Dedication, version 1.0, as
+// published by Creative Commons. To the extent possible under law, the
+// author(s) have dedicated all copyright and related and neighboring
+// rights to the Software to the public domain worldwide. The Software is
+// distributed WITHOUT ANY WARRANTY.
+//
+// If you did not receive a copy of the CC0 Public Domain Dedication
+// along with the Software, see
+// <http://creativecommons.org/publicdomain/zero/1.0/>
+
+/// <summary>
+/// History List display module.
+/// </summary>
+namespace History_32_List
+{
+    // Directives
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.Drawing;
+    using System.Windows.Forms;
+    using PdBets;
+
+    /// <summary>
+    /// Main form.
+    /// </summary>
+    [Export(typeof(IPdBets))]
+    public partial class MainForm : Form, IPdBets
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="History_32_List.MainForm"/> class.
+        /// </summary>
+        public MainForm()
+        {
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Processes incoming input and bet strings.
+        /// </summary>
+        /// <param name="inputString">Input string.</param>
+        /// <param name="betString">Bet string.</param>
+        /// <returns>>The processed input string.</returns>
+        public string Input(string inputString, string betString)
+        {
+            // Return passed input string
+            return inputString;
+        }
+
+        /// <summary>
+        /// Raises the history list box draw item event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnHistoryListBoxDrawItem(object sender, DrawItemEventArgs e)
+        {
+			
+        }
+
+        /// <summary>
+        /// Raises the copy tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnCopyToolStripMenuItemClick(object sender, EventArgs e)
+        {
+			
+        }
+
+        /// <summary>
+        /// Raises the save tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnSaveToolStripMenuItemClick(object sender, EventArgs e)
+        {
+			
+        }
+    }
+}
